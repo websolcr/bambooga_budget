800bd39229db6e169732947a3148533381098b2d
sureshhemal <sureshhemal123@gmail.com>
2025-03-16 18:52:17 +0530

    income frequency attribute

--- app/Domain/IncomeSources/Models/IncomeSource.php
+++ app/Domain/IncomeSources/Models/IncomeSource.php
@@ -3,12 +3,26 @@
 namespace App\Domain\IncomeSources\Models;
 
 use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class IncomeSource extends BaseModel
 {
+    public const PAYMENT_CYCLES = [
+        'MONTHLY' => 1,
+        'ONE_TIME' => 2,
+    ];
+
     public function amountDetails(): HasMany
     {
         return $this->hasMany(AmountDetailOfIncomeSource::class);
+    }
+
+    public function paymentCycle(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => $value ? array_search($value, self::PAYMENT_CYCLES) : null,
+            set: fn ($value) => $value ? self::PAYMENT_CYCLES[$value] : null,
+        );
     }
 }
--- database/migrations/2025_03_06_183838_create_amount_and_valid_date_of_income_sources_table.php
+++ database/migrations/2025_03_06_183838_create_amount_and_valid_date_of_income_sources_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignUuid('income_source_id')->constrained();
             $table->unsignedSmallInteger('amount');
             $table->date('valid_from');
-            $table->date('valid_end');
+            $table->date('valid_end')->nullable();
             $table->unsignedSmallInteger('payment_cycle');
             $table->timestamps();
         });
